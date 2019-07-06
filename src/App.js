e6690e55db59ea58fbd8968f351a4b8a9fe3ba21
johnSickels <46609057+johnsickels@users.noreply.github.com>
2019-07-06 00:57:58 -0400

    game functional

--- src/App.js
+++ src/App.js
@@ -2,27 +2,63 @@ import React from "react";
 import Navbar from "./components/Navbar";
 // import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
-import rockets from "./rockets.json"
+import cards from "./cards.json"
 import Card from "./components/Card";
 
 class App extends React.Component {
     state = {
-        rockets,
+        cards,
         score: 0,
-        highScore: 0
+        highscore: 0
     };
+
+    gameOver = () => {
+        if (this.state.score > this.state.highscore) {
+            this.setState({highscore: this.state.score}, function() {
+                console.log(this.state.highscore);
+            });
+        }
+        this.state.cards.forEach(card => {
+            card.count = 0;
+        });
+        alert(`Game Over :( \nscore: ${this.state.score}`);
+        this.setState({score: 0});
+        return true;
+    }
+
+    clickCount = id => {
+        // eslint-disable-next-line
+        this.state.cards.find((o, i) => {
+            if (o.id === id) {
+                if (cards[i].count === 0) {
+                    cards[i].count = cards[i].count + 1;
+                    this.setState({ score: this.state.score + 1 }, function () {
+                        console.log(this.state.score);
+                    });
+                    this.state.cards.sort(() => Math.random() - 0.5)
+                    return true;
+                } else {
+                    this.gameOver();
+                }
+            }
+        });
+    }
 
     render() {
         return (
-
             <div>
-                <Navbar />
+                <Navbar 
+                    score={this.state.score}
+                    highscore={this.state.highscore}
+                />
                 <Wrapper>
-                    {this.state.rockets.map(rocket => (
+                    {this.state.cards.map(card => (
                         <Card
-                            key={rocket.id}
-                            name={rocket.name}
-                            image={rocket.image}
+                            clickCount={this.clickCount}
+                            id={card.id}
+                            key={card.id}
+                            name={card.name}
+                            image={card.image}
                         />
                     ))}
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,24 +2,22 @@ import React from "react";
 import "./style.css";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
-function Navbar() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-              Clicky Game!
+const Navbar = props => (
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div>
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          Clicky Game!
           </li>
-          <li className="nav-item">
-              You guessed correctly!
+        <li className="nav-item">
+          You guessed correctly!
           </li>
-          <li className="nav-item">
-              Score: 0 | Top Score: 12
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
-}
+        <li className="nav-item">
+          Score: {props.score} | Top Score: {props.highscore}
+        </li>
+      </ul>
+    </div>
+  </nav>
+);
 
 export default Navbar;
